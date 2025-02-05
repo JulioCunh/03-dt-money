d21858d8e806833e984a5e4832dc2d06a0feab5c
Expert 🚀 <fcezarf@gmail.com>
2025-02-05 18:38:22 -0300

    Controlled components

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,33 +1,38 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import * as Dialog from '@radix-ui/react-dialog';
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
-import * as zod from 'zod';
+import { Controller, useForm } from 'react-hook-form';
+import * as z from 'zod';
+
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
 
-  const NewTransactionFormSchema = zod.object({
-    description: zod.string(),
-    price: zod.number(),
-    category: zod.string(),
-    //type: zod.enum(["income", "outcome"]),
-  });
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(['income', 'outcome']),
+});
 
-  type NewTransactionFormInputs = zod.infer<typeof NewTransactionFormSchema>;
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
-  export function NewTransactionModal() {
-
-    const {  
-      register, 
-      handleSubmit, 
-      formState: {isSubmitting} } 
-      = useForm<NewTransactionFormInputs>({
-      resolver: zodResolver(NewTransactionFormSchema),
-    });
-
-   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      console.log(data);
+export function NewTransactionModal() {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+    defaultValues: {
+      type: 'income'
     }
+  })
+
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(data);
+  }
 
   return (
     <Dialog.Portal>
@@ -41,35 +46,46 @@ import { zodResolver } from '@hookform/resolvers/zod';
         </CloseButton>
 
         <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
-          <input 
-          type="text" 
-          placeholder="Descrição" 
-          required {...register('description')} 
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register('description')}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register('price', { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register('category')}
           />
 
-          <input 
-          type="number" 
-          placeholder="Preço" 
-          required {...register('price',{valueAsNumber: true})}
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => {
+              return (
+                <TransactionType
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <TransactionTypeButton variant="income" value="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </TransactionTypeButton>
+                  <TransactionTypeButton variant="outcome" value="outcome">
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </TransactionTypeButton>
+                </TransactionType>
+              )
+            }}
           />
-
-          <input 
-          type="text" 
-          placeholder="Categoria" 
-          required  
-          {...register('category')}
-          />
-
-          <TransactionType>
-            <TransactionTypeButton variant="income" value="income">
-              <ArrowCircleUp size={24} />
-              Entrada
-            </TransactionTypeButton>
-            <TransactionTypeButton variant="outcome" value="outcome">
-              <ArrowCircleDown size={24} />
-              Saída
-            </TransactionTypeButton>
-          </TransactionType>
 
           <button type="submit" disabled={isSubmitting}>
             Cadastrar
